8d7213c2bbb453f9f21d0cb42c6419bf86210308
Dream <chaninthon.masai@gmail.com>
2024-09-27 17:04:27 +0700

    test

---
+++ Exam/mainp.js
@@ -0,0 +1,28 @@
+
+//  main.js 
+   
+// POST request using fetch() 
+fetch("https://206.189.146.138/api/students", { 
+      
+    // Adding method type 
+    method: "DELETE", 
+      
+    // Adding body or contents to send 
+    body: JSON.stringify({ 
+        // order: "7", 
+        id: "TOPYOEY", 
+        name: "", 
+        // status: "Posted", 
+    }), 
+      
+    // Adding headers to the request 
+    headers: { 
+        "Content-type": "application/json; charset=UTF-8"
+    } 
+}) 
+  
+// Converting to JSON 
+.then(response => response.json()) 
+  
+// Displaying results to console 
+.then(json => console.log(json)); 
